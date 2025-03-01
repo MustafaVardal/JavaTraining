61fc6b9ffe117d323d888e1f76f3a0fb81c03658
MustafaVardal <m.vardal@hotmail.com>
2025-02-28 23:34:56 -0500

    Valid-parentheses

---
+++ 03-javaExercises-valid-parentheses/src/Main.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Map;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+
+        int num = -121;
+
+
+        System.out.println(num+ "\n"+ "Palindrome number? \n" + num + " -> " + isPalindrome(num));
+
+    }
+
+    private static boolean isPalindrome(int num) {
+            if (num <0 || num!=0 && num%10==0){
+                return  false;
+            }
+
+            int reversed =0;
+
+            while (num > reversed){
+                int remainder = num %10;
+                reversed = reversed * 10 + remainder;
+                num /= 10;
+            }
+            return (num == reversed) || (num == reversed / 10);
+    }
+}
+
