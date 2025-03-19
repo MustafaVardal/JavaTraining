5d704f8d6ac2db925626bbc256b468e12cb2919f
MustafaVardal <m.vardal@hotmail.com>
2025-03-19 00:27:34 -0400

    Review  code

--- 22-javaExercises-length-of-last-word/Main.java
+++ 22-javaExercises-length-of-last-word/Main.java
@@ -1,5 +1,21 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
 
+        String a = "asdasdadsa  adsad sadsa   ";
+        List<Character> notEmpty = new ArrayList<>();
+        int count=0;
+        for (int i = a.length()-1; i>=0; i--){
+
+            if (a.charAt(i) != ' '){
+                notEmpty.add(a.charAt(i));
+                count++;
+            } else if (count > 0){
+                break;
+            }
+        }
+        System.out.println(notEmpty.size());
     }
 }
--- 22-javaExercises-length-of-last-word/RelatedDsaTopic.java
+++ 22-javaExercises-length-of-last-word/RelatedDsaTopic.java
@@ -1,6 +1,24 @@
+import java.util.*;
+
 public class RelatedDsaTopic {
 
     public static void main(String[] args) {
+        String a  = "   Hello World ";
+        System.out.println(a.trim());
+        System.out.println(a.lastIndexOf('e'));
+
+
+        List<Character> letters = new ArrayList<>();
+        for (int i = a.length()-1; i >= 0; i--){
+            if (a.charAt(i) != ' '){
+                letters.add(a.charAt(i));
+            }
+        }
+        for (char letter:letters){
+            System.out.print(letter);
+        }
+        System.out.println();
+
 
     }
 }
