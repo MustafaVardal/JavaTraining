4bd1e15a579be7f53346095a4cf7bd46ca79629c
MustafaVardal <m.vardal@hotmail.com>
2025-03-21 23:58:36 -0400

    remove Element

---
+++ 25-javaExercises-remove-element/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,3,3,4,4,5,5};
+        int val =3;
+        System.out.println(removeElement(nums, val));
+    }
+
+    public static int removeElement(int[] nums, int val){
+        int index = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != val){
+                nums[index] = nums[i];
+                index++;
+            }
+        }
+
+        return index;
+
+
+    }
+}
