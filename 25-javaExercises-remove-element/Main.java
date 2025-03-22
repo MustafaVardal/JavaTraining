ef8a426abe8f0419c36317af2c71af0cd17598b3
MustafaVardal <m.vardal@hotmail.com>
2025-03-22 00:01:34 -0400

    fixed algorithms

--- 25-javaExercises-remove-element/Main.java
+++ 25-javaExercises-remove-element/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         int[] nums = {1,2,3,3,3,4,4,5,5};
-        int val =3;
+        int val =4;
         System.out.println(removeElement(nums, val));
     }
 
@@ -10,7 +10,7 @@ public class Main {
         int index = 0;
 
         for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != val){
+            if (nums[i] == val){
                 nums[index] = nums[i];
                 index++;
             }
