ad17a07121ab0b4377499202f2886bd01558b588
MustafaVardal <m.vardal@hotmail.com>
2025-03-30 23:57:39 -0400

    score of string

---
+++ 33-javaExercises-scope-of-a-string/Main.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(scoreOfString("DAS"));
+    }
+
+    public static int scoreOfString(String s){
+        int total =0;
+        for (int i = 0; i < s.length()-1; i++) {
+            total += Math.abs(s.charAt(i) - s.charAt(i+1));
+        }
+        return total;
+    }
+}
