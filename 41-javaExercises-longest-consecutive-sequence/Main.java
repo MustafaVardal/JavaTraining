c9e085bfa6c5f7aebd1513837cc3d3782462ee9e
MustafaVardal <m.vardal@hotmail.com>
2025-04-08 23:54:58 -0400

    tested

--- 41-javaExercises-longest-consecutive-sequence/Main.java
+++ 41-javaExercises-longest-consecutive-sequence/Main.java
@@ -3,7 +3,7 @@ import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
-        int[] nums = {1,1,1,1,2,2,2,3,3,3};
+        int[] nums = {1,5,1,2,3,5,6,7,8};
         System.out.println(longestConsecutive(nums));
 
     }
